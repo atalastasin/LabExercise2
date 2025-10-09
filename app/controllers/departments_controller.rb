763a69ecbc6b0597f7000f089cf5f65bc9a67757
atalastasin <atalastasin@gbox.adnu.edu.ph>
2025-10-09 16:26:44 +0800

    midterms exam

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -8,8 +8,6 @@ class DepartmentsController < ApplicationController
 
   # GET /departments/1 or /departments/1.json
   def show
-    @department = Department.find(params[:id])
-    
   end
 
   # GET /departments/new
